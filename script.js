7f0ad2ec5702af83208020fa49fd2516ada1841d
erholmes <erholmes@users.noreply.github.com>
2017-05-04 11:14:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+<<<<<<< HEAD
+'use strict';
+
+const Script = require('smooch-bot').Script;
+
+module.exports = new Script({
+    processing: {
+        prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
+    start: {
+        receive: (bot) => {
+            return bot.say('Hi! I\'m Erin Bot!')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! Nice to meet you ${name}.`))
+                .then(() => 'whatNext');
+        }
+    },
+	
+	whatNext: {
+        prompt: (bot) => bot.say('What would you like to do next? %[Learn about Erin](postback:learn_about_erin) or %[Tweet Erin](http://ow.ly/FKaT30bqNaS)')
+                .then(() => 'finish');
+        }
+    },
+	
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
+        }
+    }
+});
+=======
 'use strict';
 
 const Script = require('smooch-bot').Script;
@@ -45,3 +89,4 @@ Would you like to send me a tweet? %[Yes](postback:yes) %[No](postback:no)`))
         }
     }
 });
+>>>>>>> origin/master
